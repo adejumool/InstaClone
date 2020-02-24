2165e7e3bc44d30112fabc608b2aa74f5b51b8f4
Tobi <adejumool@berea.edu>
2020-02-23 20:49:48 -0500

    adding connection to heroku

--- app/src/main/java/com/example/instatobi/LoginActivity.java
+++ app/src/main/java/com/example/instatobi/LoginActivity.java
@@ -2,17 +2,23 @@ package com.example.instatobi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
     private EditText etUsername;
-    private EditText etPasword;
+    private EditText etPassword;
     private Button btnLogin;
 
     @Override
@@ -21,14 +27,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         etUsername = findViewById(R.id.etUsername);
-        etPasword = findViewById(R.id.etPassword);
+        etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "onClick login button");
                 String username = etUsername.getText().toString();
-                String password = etPasword.getText().toString();
+                String password = etPassword.getText().toString();
                 loginUser(username,password);
             }
         });
@@ -36,5 +42,22 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "Atempting to login user");
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                    if (e != null) {
+                    Log.e(TAG, "Issue with login",e);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "success!",Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
